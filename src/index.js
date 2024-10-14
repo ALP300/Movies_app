3c4ae708818e84c69820d467a6127caffead7cd6
ALP300 <aitorpalomino1@gmail.com>
2024-10-13 20:23:49 -0500

    Realizada la conexión a base de datos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import {dirname, join} from 'path'
 import { fileURLToPath } from 'url'
 import indexRouter from './router/index.js'
+import { Conectar } from './public/services/conexion.js'
 const app= express()
 
 const __dirname= dirname(fileURLToPath(import.meta.url))
@@ -15,7 +16,7 @@ app.use(express.static(join(__dirname,'public')))
 
 app.set('view engine', 'ejs')
 
-
+Conectar()
 
 app.listen(3000)
 console.log("El servidor está siendo escuchado en el puerto: ",3000)
