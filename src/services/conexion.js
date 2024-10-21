2903789318227ef83d4332998515e26c3d63b42e
ALP300 <aitorpalomino1@gmail.com>
2024-10-20 22:13:13 -0500

    Api agregar

--- src/services/conexion.js
+++ src/services/conexion.js
@@ -12,11 +12,21 @@ const config={
     }
 }
 
-export async function Conectar(params) {
+export async function Conectar() {
     const cliente= new Client(config)
     try{
         await cliente.connect()
         console.log("Conectado a la base de Datos")
+    }catch(error){
+        console.log(error)
+    }
+}
+export async function TraerPeliculas() {
+    const cliente= new Client(config)
+    try{
+        await cliente.connect()
+        const res= await cliente.query('SELECT * FROM peliculas')
+        return res.rows
     }catch(error){
         console.log(error)
     }
